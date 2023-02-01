6ecebf1b0b712bc15db1b438ea9cccb54e2a861b
Alexander Huletski <jukola@bk.ru>
2023-02-01 21:22:52 +0300

    added code three

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,18 +1,24 @@
 ﻿/*
 string userName = Console.ReadLine();
 Console.WriteLine("Hello, " + userName + "!");
-*/
+
 
 // code 2
+
 int numberOne = 3;
 int numberTwo = 5;
 Console.WriteLine(numberOne + numberTwo);
 
 
 
-
+*/
 //code 3
-
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
 
 
 
