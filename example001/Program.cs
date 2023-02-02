09494387cf9c7ef5e9bd6fb34bc98b3399217b6e
Huletski <jukola@bk.ru>
2023-02-02 15:23:53 +0300

    was added code 6

--- example001/Program.cs
+++ example001/Program.cs
@@ -40,7 +40,7 @@ string name = Console.ReadLine();
     Console.WriteLine("Greeting " + name);
  }
 
- */
+
 
  // code 5
 
@@ -59,3 +59,32 @@ if (c > max) max = c;
 if (d > max) max = d;
 if (e > max) max = e;
 Console.Write(max);
+
+*/
+
+
+
+int a1 = 1;
+int b1 = 2;
+int c1 = 6;
+int a2 = 1111;
+int b2 = 21;
+int c2 = 62;
+int a3 = 13;
+int b3 = 277;
+int c3 = 62;
+
+int max = a1;
+
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
